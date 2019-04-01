c8e9b94e9a1cf03630d75a4d69a5f37d4bd8a5d5
201671010436 <48074562+201671010436@users.noreply.github.com>
2019-04-01 16:08:21 +0800

    Add files via upload

---
+++ paixu.java
@@ -0,0 +1,14 @@
+		//°´×ÖµäÊä³ö
+    public String[] Sort(String[] word3) {
+        String temp;
+        for (int i = 0; i < word3.length; i++) {
+            for (int j = word3.length - 1; j > i; j--) {
+                if (word3[j - 1].compareTo(word3[j]) > 0) {
+                    temp = word3[j - 1];
+                    word3[j - 1] = word3[j];
+                    word3[j] = temp;
+                }
+            }
+        }
+        return word3;
+    }
