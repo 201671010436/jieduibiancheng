c4ca08c2c9a0dfa8b335218ede6005b57c724324
201671010436 <48074562+201671010436@users.noreply.github.com>
2019-04-01 20:54:01 +0800

    Add files via upload

---
+++ tongji.java
@@ -0,0 +1,11 @@
+//统计单词出现的频率
+HashMap<String, Integer> map = new HashMap();
+for (String str : sortword) {
+    if (!map.containsKey(str)) {//当str不存在,
+        map.put(str, 1);
+    } else {
+        //否则获得c的值并且加1
+        map.put(str, map.get(str) + 1);
+    }
+    //System.out.println(str + "出现的次数为:" + map.get(str)+"次");
+}
