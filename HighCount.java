e245df5dbfdfd20158e9f61b771e2c1d3891e395
201671010436 <48074562+201671010436@users.noreply.github.com>
2019-04-01 21:32:11 +0800

    修改代码
    代码运行有误，需要重新设计

--- HighCount.java
+++ HighCount.java
@@ -1,21 +1,16 @@
+//高频词统计
 package WordTest;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Map;
 import java.util.Map.Entry;
-
 public class HighCount {
-
 	public void SortMap(Map<String,Integer> oldmap,int n){  
         ArrayList<Map.Entry<String,Integer>> list = new ArrayList<Map.Entry<String,Integer>>(oldmap.entrySet());
-        
         Collections.sort(list,new Comparator<Map.Entry<String,Integer>>(){  
             @Override  
-            public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {  
-                return o2.getValue() - o1.getValue();
-            }  
+            public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) 
         }); 
         for(int i = 0; i<n; i++){  
             System.out.println(list.get(i).getKey()+ ": " +list.get(i).getValue());  
