901b6089ccac4c782f784cb1d790384eb47a36e4
201671010436 <48074562+201671010436@users.noreply.github.com>
2019-04-01 21:33:23 +0800

    修改完毕
    高频词统计模块已经修改并运行，正常。

--- HighCount.java
+++ HighCount.java
@@ -10,7 +10,9 @@ public class HighCount {
         ArrayList<Map.Entry<String,Integer>> list = new ArrayList<Map.Entry<String,Integer>>(oldmap.entrySet());
         Collections.sort(list,new Comparator<Map.Entry<String,Integer>>(){  
             @Override  
-            public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) 
+            public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {  
+                return o2.getValue() - o1.getValue();
+            }  
         }); 
         for(int i = 0; i<n; i++){  
             System.out.println(list.get(i).getKey()+ ": " +list.get(i).getValue());  
