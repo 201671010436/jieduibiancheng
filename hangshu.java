8fd1215c81ec38b031820b41be2ec5ea121f8a21
201671010436 <48074562+201671010436@users.noreply.github.com>
2019-04-01 15:25:43 +0800

    Add files via upload

---
+++ hangshu.java
@@ -0,0 +1,29 @@
+package a.a;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+ 
+public class hangshu 
+{
+    public static void main(String[] args)
+    {
+    	try{
+    		File file =new File("src/word.txt");
+    		if(file.exists()){
+    		    FileReader fr = new FileReader(file);
+    		    LineNumberReader lnr = new LineNumberReader(fr);
+    		    int linenumber = 0;
+    	            while (lnr.readLine() != null){
+    	        	linenumber++;
+    	            }
+    	            System.out.println("此文本的行数为：  " + linenumber);
+    	            lnr.close();
+    		}else{
+    			 System.out.println("File does not exists!");
+    		}
+    	}catch(IOException e){
+    		e.printStackTrace();
+    	}
+    }
+}
